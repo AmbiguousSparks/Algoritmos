0cf53ad4564214dc25c64b5a39ab6988d60dba99
= <=>
2021-03-12 12:01:46 -0300

    Adicionar arquivos de projeto.

---
+++ Algoritmos/Program.cs
@@ -0,0 +1,18 @@
+﻿using AlgoritmosCore;
+using System;
+using System.Collections.Generic;
+
+namespace AlgoritmosConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>() { 10, 20, 30, 40, 50, 60, 70 };
+            Dictionary<int, int> grupos = Algoritmos.GroupNumbersByIntervals(numeros, 7);
+            foreach(KeyValuePair<int, int> grupo in grupos)
+                System.Console.WriteLine(grupo);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlgoritmosCore/Algoritmos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmosCore
+{
+    public class Algoritmos
+    {
+        public static Dictionary<int, int> GroupNumbersByIntervals(List<int> numeros, int interval)
+        {
+            //Determinando o número de grupos a serem criados
+            int numGroups = numeros.Count / interval;
+            //Determinando se sobram numeros
+            int leftovers = numeros.Count % interval;
+            Dictionary<int, int> groups = new Dictionary<int, int>(numGroups);
+            //Enquanto ainda existirem grupos à serem criados
+            while (numGroups > 0)
+            {
+                groups.Add(numeros[0], numeros[interval - 1]);
+                numeros.RemoveRange(0, interval);
+                numGroups--;
+            }
+            //Se sobrarem números, adicione-os a um novo grupo
+            if (leftovers > 0)
+                groups.Add(numeros.First(), numeros.ElementAt(leftovers - 1));
+
+            return groups;
+        }
+    }
+}
